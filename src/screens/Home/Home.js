3dcbb9b47b00ccb316299f06953d08ddba08df27
Raphael Tony <prince4raphael@gmail.com>
2018-02-20 23:44:14 +0530

    Added functionality to check for connectivity

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Container, Header, Body, Title, Icon, Left, Right } from "native-base";
-import { StatusBar } from "react-native";
+import { StatusBar, NetInfo } from "react-native";
 import MessageText from "../../components/MessageText";
 import Bubble from "../../components/Bubble";
 import InputToolbar from "../../components/InputToolbar";
@@ -59,17 +59,53 @@ export default class Home extends Component {
   }
 
   sendMess = messages => {
-    axios
-      .post("https://api.beady27.hasura-app.io/wit", {
-        Input: messages["0"].text
-      })
-      .then(response => {
+    NetInfo.isConnected.fetch().then(isConnected => {
+      if (isConnected) {
+        console.log("Internet is connected");
+        axios
+          .post("https://api.beady27.hasura-app.io/wit", {
+            Input: messages["0"].text
+          })
+          .then(response => {
+            this.setState(previousState => {
+              return {
+                id: previousState.id + 1,
+                data: GiftedChat.append(previousState.data, {
+                  _id: this.state.id,
+                  text: response.data.Response,
+                  createdAt: new Date(),
+                  user: {
+                    _id: 1
+                  }
+                })
+              };
+            });
+          })
+          .catch(error => {
+            console.log(error);
+            if (error.response.status >= 500) {
+              this.setState(previousState => {
+                return {
+                  id: this.state.id + 1,
+                  data: GiftedChat.append(this.state.data, {
+                    _id: this.state.id,
+                    text: "Something went wrong in the backend. Sorry !",
+                    createdAt: new Date(),
+                    user: {
+                      _id: 1
+                    }
+                  })
+                };
+              });
+            }
+          });
+      } else {
         this.setState(previousState => {
           return {
-            id: previousState.id + 1,
-            data: GiftedChat.append(previousState.data, {
+            id: this.state.id + 1,
+            data: GiftedChat.append(this.state.data, {
               _id: this.state.id,
-              text: response.data.Response,
+              text: "Looks like you're not connected to the internet",
               createdAt: new Date(),
               user: {
                 _id: 1
@@ -77,39 +113,8 @@ export default class Home extends Component {
             })
           };
         });
-      })
-      .catch(error => {
-        console.log(error);
-        if (error.response.status === 503) {
-          this.setState(previousState => {
-            return {
-              id: this.state.id + 1,
-              data: GiftedChat.append(this.state.data, {
-                _id: this.state.id,
-                text: "Just a min. I'm waking up",
-                createdAt: new Date(),
-                user: {
-                  _id: 1
-                }
-              })
-            };
-          });
-        } else {
-          this.setState(previousState => {
-            return {
-              id: this.state.id + 1,
-              data: GiftedChat.append(this.state.data, {
-                _id: this.state.id,
-                text: "Looks like you are not connected to the internet",
-                createdAt: new Date(),
-                user: {
-                  _id: 1
-                }
-              })
-            };
-          });
-        }
-      });
+      }
+    });
   };
 
   renderMessageText(props) {
